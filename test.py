27d56771a255575a0d1e62d9174a45b9132cff22
Donhua <suvorov.mo@yandex.ru>
2021-09-27 17:49:58 +0300

    Добавил тестовый тест.

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3.9
+import unittest
+from Game import Game
 
+class TestSum(unittest.TestCase):
+
+    def test_list(self):
+        result = 1
+        etalon = 1
+        self.assertEqual(result, etalon)
+
+if __name__ == "__main__":
+    unitest.main()
