7fc2587257e94194892f89cd5ac8ed3c0b91d4b0
Donhua <suvorov.mo@yandex.ru>
2021-09-27 17:33:52 +0300

    Удалил лишние классы. Обновил файл для тестов

--- test.py
+++ test.py
@@ -1,24 +1,2 @@
-import random
-from functools import partial
-from tkinter import*
-
-
-def change_label_number(num1):
-    for j in range(num1):
-        arr.append(PhotoImage(file=f"img1\\autumn_dise_1_{j + 1}.png"))
-        random.shuffle(arr)
-    for s in range(num1):
-        Label(image=arr[s]).grid(row=1, column=i)
-
-
-tk = Tk()
-arr = []
-num = 4
-for i in range(num):
-    arr.append(PhotoImage(file=f"img1\\autumn_dise_1_{i+1}.png"))
-    random.shuffle(arr)
-for i in range(num):
-    Label(tk, image=arr[i]).grid(row=1, column=i)
-Button(tk, text='Ок', command=partial(change_label_number, num)).grid(row=2, columnspan=num)
-tk.mainloop()
+#!/usr/bin/env python3.9
 
